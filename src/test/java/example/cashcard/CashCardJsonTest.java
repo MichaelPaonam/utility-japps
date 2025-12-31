da2f94435bd1adf54ab3eea7b2d5cc1643c7c742
Michael Paonam <mazdaswift@gmail.com>
2025-12-31 16:39:27 +0530

    chore: added missing import and the test passes

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -3,6 +3,7 @@ package example.cashcard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
 
