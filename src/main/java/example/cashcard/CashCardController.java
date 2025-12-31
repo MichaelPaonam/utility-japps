26f7a422f65d3a6eaf8ab3ea06d499e0f18b04a9
Michael Paonam <mazdaswift@gmail.com>
2025-12-31 17:24:42 +0530

    chore: checking if the id field in the api response is null or not

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 class CashCardController {
     @GetMapping("/{requestedById}")
-    private ResponseEntity<String> findById() {
-        return ResponseEntity.ok("{}");
+    private ResponseEntity<CashCard> findById() {
+        CashCard cashCard = new CashCard(1000L, 0.0);
+        return ResponseEntity.ok(cashCard);
     }
 }
