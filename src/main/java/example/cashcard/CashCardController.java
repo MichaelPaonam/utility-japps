c203b089a2bdcd093281650060ab2d9e06372eca
Michael Paonam <mazdaswift@gmail.com>
2025-12-31 17:17:48 +0530

    chore: created CashCardController without making it a web-controller

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,9 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+
+class CashCardController {
+    private ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
