50e7b02e0307fcc363de24c8ee63d7b8e06c0762
Michael Paonam <mazdaswift@gmail.com>
2025-12-31 17:27:52 +0530

    chore: check the value of amount in the response

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -25,6 +25,9 @@ class CashCardApplicationTests {
 		DocumentContext documentContext = JsonPath.parse(response.getBody());
 		Number id = documentContext.read("$.id");
 		assertThat(id).isEqualTo(99);
+
+		Double amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
 	}
 
 }
