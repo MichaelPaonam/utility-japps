38126863411a93da8152fd8008d4b5878fe769c5
Michael Paonam <mazdaswift@gmail.com>
2025-12-31 16:42:04 +0530

    chore: removed addition assert statement

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -23,6 +23,5 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
-        assertThat(42).isEqualTo(42);
     }
 }
