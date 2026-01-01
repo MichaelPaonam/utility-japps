4d5b7e73b7dda9027cdc14f7af25da60143d961f
Michael Paonam <mazdaswift@gmail.com>
2026-01-01 20:21:23 +0530

    chore: add failing test shouldRejectUsersWhoAreNotCardOwners

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -133,4 +133,13 @@ class CashCardApplicationTests {
 		assertThat(amounts).containsExactlyInAnyOrder(1.00, 123.45, 150.00);
 	}
 
+	@Test
+	void shouldRejectUsersWhoAreNotCardOwners() {
+		ResponseEntity<String> response = restTemplate
+				.withBasicAuth("hank-owns-no-cards", "qrs456")
+				.getForEntity("/cashcards/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+	}
+
 }
