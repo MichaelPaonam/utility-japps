d8a57f95be185926c205f21ecbba2da8a62b9f13
Michael Paonam <mazdaswift@gmail.com>
2026-01-01 23:47:25 +0530

    chore: add assert to check if the data is deleted or not

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -208,6 +208,9 @@ class CashCardApplicationTests {
 								.withBasicAuth("sarah1", "abc123")
 								.exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+		ResponseEntity<String> getResponse = restTemplate.getForEntity("/cashcards/99", String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 }
