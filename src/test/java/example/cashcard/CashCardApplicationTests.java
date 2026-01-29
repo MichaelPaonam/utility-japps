b61032dabb8c58187bd1642be55d864519897689
Michael Paonam <mazdaswift@gmail.com>
2026-01-30 01:02:56 +0530

    require DirtiesContext

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -18,7 +18,7 @@ import java.net.URI;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 
 	@Autowired
